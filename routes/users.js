f450372d18e15529284a6b88f3aaa80cc3f8cc3d
jinkai yang <jinkaiyang@miit-20160324wn.mioffice.cn>
2017-10-18 10:58:21 +0800

    下一步获取联系人列表

--- routes/users.js
+++ routes/users.js
@@ -6,6 +6,8 @@ var fs = require('fs');
 var http=require('http'); 
 var https=require('https');
 var querystring=require('querystring'); 
+var request = require('request');
+
 var storage = multer.diskStorage({
   destination: function (req, file, cb) {
     cb(null, 'public/excles/')
@@ -86,434 +88,57 @@ router.get('/wxlogin', function(req, res, next) {
 	 	});
 	}
 	function getWxData(pass_ticket,skey,sid,uin){
-		var option={
-		    hostname:'wx.qq.com',
-		    path:'/cgi-bin/mmwebwx-bin/webwxinit?r=-713029383&pass_ticket='+pass_ticket,
-		    method:'POST',
-		    headers:{
-				'Content-Type':'text/plain;charset=UTF-8'
-		    }
-		  };
-	 var hreq = https.request(option,function(ress){  
-	    var str = '';
-	    ress.on('end',function(){
-	    	console.log('getWxData===='+str);
-		    //res.send(pass_ticket)
-	    });  
-	    ress.on('data',function(chunk){
-	    	str+=chunk;
-	    });  
-	});
-  	hreq.on('error', (e) => {
-	  console.error(e);
-	});
-	var BaseRequest = {
-	  	"DeviceID":"e734118103793217",
-		"Sid":sid,
-		"Skey":skey,
-		"Uin":uin
-	}
-	var json = {
-		"BaseRequest": BaseRequest//querystring.stringify(BaseRequest)
-	}
-	
-	var postData = querystring.stringify(json);
-	console.log(postData);
-	//hreq.write(postData);
-	hreq.end();
-
-	}
-});
-
-/* GET users listing. */
-router.post('/uploads',upload.single('myfile'), function(req, res, next) {
-	console.log('!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
-	var queryCount = 0;
-	var myfilename = req.file.originalname;
-	var datatype = 0;
-	var start = '北京市';
-	var stop = '西藏自治区';
-	var startCity = '北京';
-	var stopCity = '北京';
-	var rowIndex = 0;
-	var xlRows = [];
-	var tempRows = [];
-	var datatype = req.body.type == 1 ? 1 : 0;
-	console.log('类型==='+datatype)
-	var queryNoCount = 0;
-	var startTime = Date.now();
-	var timeoutCount = 0;
-	var xyQueryCount = 0;
-	var isover = false;
-	console.log(req.file.originalname);
-	xlRows = xlsx.parse('public/excles/'+req.file.originalname)[0].data;
-	su(1, xlRows[rowIndex][0]);
-	
-	function find(str,cha,num){
-    var x=str.indexOf(cha);
-    for(var i=0;i<num;i++){
-        x=str.indexOf(cha,x+1);
-    }
-    return x;
-    }
-
-	//查询精确地址
-	function su(type,word){
-		console.log('+++++++++'+word);
-		var data = {
-			wd:word,
-			cid:131,
-			type:0,
-			newmap:1,
-			b:'(8135923.805,432968.26999999955;16319731.805,9362248.27)',
-			t:Date.now(),
-			pc_ver:2
+		var baseRequest = {
+			DeviceID:"e734118103793217",
+			Sid:sid,
+			Skey:skey,
+			Uin:uin
 		}
-		var parm = querystring.stringify(data);
-		var waitCount = 0;
-		var queryTimer = setTimeout(function(){
-			if (waitCount > 0){
-				console.log('超时--------------');
-				timeoutCount++;
-		  	waitCount = 0;
-		  	queryError();
-		    return;
-		  }
-		}, 3000); 
-		//创建请求  
-		var hreq=http.request('http://map.baidu.com/su?'+parm,function(ress){  
-		    ress.setEncoding('utf-8');  
-		    var str ='';
-		    waitCount ++;
-		    ress.on('end',function(){
-		    	waitCount = 0;
-		    	clearTimeout(queryTimer);
-		    	try{
-		    		var result = JSON.parse(str);
-			    	if(result.s.length <= 0 ){
-			    		//查询不到
-			    		if(word.length <= 0) {
-			    			//递归结束
-			    			console.log('su没有结果======'+type)
-					    	queryError();
-			    		}else{
-			    			//递归删除查询
-			    			var w = word.slice(0,word.length-1);
-			    			console.log('递归查询---'+w)
-			    			if(w.length){
-			    				su(type,w);
-			    			}else{
-			    				queryError();
-			    			}
-			    		}
-			    	}else{
-			    		//起点搜索
-			    		if(type == 1){	
-			    			if(!result.p){
-			    				var w = result.s[0];
-			    				var start_i = find(w,'$',2);
-			    				var end_i = find(w,'$',3);
-			    				startCity = w.substring(0,start_i).replace(/\$/g,'');
-			    				start = w.substring(start_i,end_i).replace(/\$/g,'');
-			    				//start = result.s[0].split(/\$\d/)[0].split('$$')[1].replace(/\$/g,'');
-			    				//startCity = result.s[0].split(/\$\d/)[0].split('$$')[0].replace(/\$/g,'');
-			    			}else{
-			    				start = word;
-			    				startCity = "";
-			    			}
-			    			
-				    		//是否关联。第二列是不是第一列的下属
-				    		if(datatype == 1){
-				    			var w2 = xlRows[rowIndex][0] + '' + xlRows[rowIndex][1];
-				    			su(2,w2);
-				    		}else{
-				    			su(2,xlRows[rowIndex][1]);
-				    		}		    		
-				    	}
-				    	//终点搜索
-				    	if(type == 2){
-				    		if(!result.p){
-				    			var w = result.s[0];
-				    			var stop_i = find(w,'$',2);
-			    				var stopend_i = find(w,'$',3);
-			    				stopCity = w.substring(0,stop_i).replace(/\$/g,'');
-			    				stop = w.substring(stop_i,stopend_i).replace(/\$/g,'');
-
-				    			//stop = result.s[0].split(/\$\d/)[0].split('$$')[1].replace(/\$/g,'')
-			    				//stopCity = result.s[0].split(/\$\d/)[0].split('$$')[0].replace(/\$/g,'');
-				    		}else{
-				    			stop = word
-			    				stopCity ="";
-				    		}
-			    			
-				    		//stop = result.s[0].split(/\$\d/)[0].replace(/\$/g,'');
-				    		queryLens()
-				    	}
-			    	}
-		    	}catch(err){
-		    		console.log(err);
-		    		if(err instanceof SyntaxError){
-		    			console.log('SyntaxError-query');
-		    			queryError();
-		    		} else if( err instanceof TypeError){
-		    			console.log('TypeError-query');
-		    			queryError();
-		    		}
-		    	}
-		    });  
-		    ress.on('data',function(chunk){ 
-		    	str += chunk;
-		    });  
-		});  
-		hreq.on('error',function(err){
-			console.log('---su---error------')
-			queryError();  
-		}); 
-		hreq.end(); 
-	}
-	var sq= '';
-	var eq = '';
-	var sn = '';
-	var en = '';
-	//查询距离
-	function queryLens(){
-		if(xyQueryCount > 2){
-			queryError();
-			return;
+		var json = {
+				"BaseRequest":baseRequest
 		}
-		if(queryCount == 2){
-			var data={  
-	    newmap:1,
-			reqflag:'pcmap',
-			biz:1,
-			from:'webmap',
-			da_par:'direct',
-			pcevaname:'pc4.1',
-			qt:'nav',
-			c:74,
-			navtp:4,
-			sn:sn,
-			en:en,
-			sq:sq,
-			eq:eq,
-			sc:74,
-			ec:74,
-			mrs:1,
-			drag:0,
-			version:4,
-			route_traffic:1,
-			sy:0,
-			da_src:'pcmappg.routeaddr',
-			extinfo:63,
-			tn:'B_NORMAL_MAP',
-			nn:0,
-			u_loc:'12951465,4842932',
-			ie:'utf-8',
-			l:12,
-			b:'(12937545,4796788;12988809,4866548)',
-			t:Date.now()
-		}; 
-	}else{
-		 var sn1 ='';
-		 var en1= '';
-		 if(startCity){
-		 	sn1= '2$$$$$$'+start+'$$1$$'+startCity+'$$'
-		 }else{
-		 	sn1 = '2$$$$$$'+start+'$$0$$$$'
-		 }
-		 if(stopCity){
-		 	en1= '2$$$$$$'+stop+'$$1$$'+stopCity+'$$'
-		 }else{
-		 	en1 = '2$$$$$$'+stop+'$$0$$$$'
-		 }
-		var data={  
-	    newmap:1,
-			reqflag:'pcmap',
-			biz:1,
-			from:'webmap',
-			da_par:'direct',
-			pcevaname:'pc4.1',
-			qt:'nav',
-			c:1,
-			sn:sn1,
-			en:en1,
-			sc:1,
-			ec:1,
-			pn:0,
-			rn:5,
-			mrs:1,
-			version:4,
-			route_traffic:1,
-			sy:0,
-			da_src:'pcmappg.searchBox.button',
-			extinfo:63,
-			tn:'B_NORMAL_MAP',
-			nn:0,
-			u_loc:'12951465,4842932',
-			ie:'utf-8',
-			l:12,
-			b:'(12937545,4796788;12988809,4866548)',
-			t:Date.now()
-		}; 
-	}  
-		var content = querystring.stringify(data);
-		var waitCount = 0;
-		var queryTimer = setTimeout(function(){
-			if(isover)
-				return
-			if (waitCount > 0){
-				console.log('超时--------------');
-				timeoutCount++;
-		  	waitCount = 0;
-		  	queryError();
-		    return;
-		  }
-		}, 3000);
-
-		//创建请求  
-		try{
-				var hreq = http.request('http://map.baidu.com?'+content,function(ress){  
-		    ress.setEncoding('utf-8');  
-		    var str = '';
-		    waitCount ++;
-		    ress.on('end',function(){
-		    	waitCount = 0;
-		    	clearTimeout(queryTimer); 
-		    	try{
-		    		var result = JSON.parse(str);
-			    	var len = 0;
-			    	if(result.content && result.content.routes){
-			    		len = result.content.routes[0].legs[0].distance;
-			    	}else if(result && result.content && result.content.length > 1){
-			    		var s = result.content[0];
-			    		var e = result.content[1];
-			    		if(queryCount == 2){
-			    			console.log('查询失败，直接跳过>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>');
-			    			queryNoCount++;
-			    		}else if(queryCount == 0 && s.length > 0 && e.length > 0){
-			    			start = s[0].name || s[0].addr;
-			    			stop = e[0].name || e[0].addr;
-			    			queryCount++;
-			    			queryLens();
-			    			return;
-			    		}else if(queryCount == 1 && s.length > 0 && e.length > 0){
-			    			console.log('普通查询没有查到-进行坐标查询---------------------')
-			    			if(!s[0].geo || !e[0].geo){
-			    				if(!s[0].geo){
-				    				start = s[0].name+start;
-				    			}
-				    			if(!e[0].geo){
-				    				stop = e[0].name+stop;
-				    			}
-			    				queryCount = 0;
-			    				xyQueryCount++;
-				    			queryLens();
-				    			
-				    			return;
-			    			}
-			    			
-			    			var x1y1 = s[0].geo.split('|')[2].replace(';','');
-			    			var x2y2 = e[0].geo.split('|')[2].replace(';','');
-
-			    			sn = '1$$'+s[0].uid+'$$'+x1y1 + '$$'+ s[0].name + '$$$$$$';
-			    			en = '1$$'+e[0].uid+'$$'+x2y2 + '$$'+ e[0].name + '$$$$$$';
-			    			sq = start;
-			    			eq = stop;
-			    			queryCount++;
-			    			queryLens();
-			    			
-			    			return;
-			    		}
-			    		// }else if(queryCount == 2 && s.length > 0 && e.length > 0){
-			    		// 	console.log('第三次查询距离:::::'+start+'----'+stop)
-			    		// 	if(datatype == 1){
-				    	// 		var w2 = xlRows[rowIndex][0] + '' + xlRows[rowIndex][1];
-				    	// 		su(2,w2)
-				    	// 	}else{
-				    	// 		su(2,xlRows[rowIndex][1])
-				    	// 	}		 
-			    		// 	queryCount++;
-			    		// 	return;
-			    		// }
-			    	}
-			    	console.log(rowIndex + '::::::::' + start + '<--->' + stop + '::::::距离:::' + len/1000);
-			    	var arr = [];
-				    	arr.push(xlRows[rowIndex][0]);
-				    	arr.push(xlRows[rowIndex][1]);
-				    	arr.push(len/1000);
-				    tempRows.push(arr);
-			    	rowIndex++;
-			    	queryCount = 0;
-			    	xyQueryCount = 0;
-			    	if(rowIndex == xlRows.length){
-			    		waitCount = 0;
-		    			clearTimeout(queryTimer); 
-			    		over();
-			    	}else{
-			    		su(1,xlRows[rowIndex][0])
-			    	}
-		    	}catch(err){
-		    		console.log('catch-error-query');
-		    		console.log(err);
-		    		if(err instanceof SyntaxError){
-		    			console.log('SyntaxError-query');
-		    			queryError();
-		    		} else if( err instanceof TypeError){
-		    			console.log('TypeError-query');
-		    			queryError();
-		    		}
-		    	}
-		    });  
-		    ress.on('data',function(chunk){
-		    	str+=chunk;
-		    });  
-		});  
-		hreq.on('error',function(err){
-		  console.error(err);  
-		  queryError();
-		}); 
-		hreq.end();
-		}catch(error){
-			console.log('post-error');
-			console.log(error);
+		request.post({
+		  headers: {'content-type' : 'text/plain;charset=UTF-8'},
+		  url:'https://wx.qq.com/cgi-bin/mmwebwx-bin/webwxinit?r=-713029383&pass_ticket='+pass_ticket,
+		  json:json
+		}, function(error, response, body){
+			console.log('getWxData..........')
+		  var myUserName = body.User.UserName;
+		  console.log(body.User);
+		  console.log(myUserName)
+		  getAllUsers(pass_ticket,skey);
+		  //postMsg(myUserName,'@b63f16520526d2fadcfaa4abf2d39a7a78458101f34eddefa69bb6aa712cc664','~',pass_ticket,baseRequest);
+		});
+	}
+	function getAllUsers(pass_ticket,skey){
+		console.log(pass_ticket,skey);
+		request.get({
+		  url:'https://wx.qq.com/cgi-bin/mmwebwx-bin/webwxgetcontact?lang=zh_CN&pass_ticket='+pass_ticket+'&r='+Date.now()+'&seq=0&skey='+skey
+		}, function(error, response, body){
+			console.log('getAllUsers..........')
+		  console.log(body);
+		});
+	}
+	function postMsg(myUserName,toUserName,msg,pass_ticket,baseRequest){
+		var json = {
+				BaseRequest:baseRequest,
+				Msg:{
+					ClientMsgId:Date.now(),
+					Type:1,
+					ToUserName:toUserName,
+					LocalID:Date.now(),
+					FromUserName:myUserName,
+					Content:msg
+				}
 		}
-		
-	}
-
-	function queryError(){
-		console.log('------------------queryError');
-  	rowIndex++;
-  	queryCount = 0;
-  	xyQueryCount = 0;
-  	queryNoCount++;
-  	if(rowIndex < xlRows.length - 1){
-  		var arr = [];
-    	arr.push(xlRows[rowIndex][0]);
-    	arr.push(xlRows[rowIndex][1]);
-    	arr.push(0);
-    	tempRows.push(arr);
-  		su(1,xlRows[rowIndex][0])
-  	}else{
-  		console.log('==>>>>>>>>查询结束:::'+ queryNoCount + '条查询失败')
-  		over();
-  	}
-	}
-	function over(){
-		isover = true;
-		console.log(queryNoCount + '条查询失败')
-		var m = parseInt((Date.now() - startTime)/1000/60, 10);
-		var s = parseInt((Date.now() - startTime)/1000%60, 10);
-		var useTime = m + '分' + s +'秒';
-		var buffer = xlsx.build([
-	    {
-	        name:'sheet1',
-	        data:tempRows
-	    }        
-		]);
-		//将文件内容插入新的文件中
-		var filePath = '/excles/'+myfilename + Date.now()+'.xlsx';
-		fs.writeFileSync('public'+filePath,buffer,{'flag':'w'});
-		res.send('<h3>总共查询'+rowIndex+'条，其中'+queryNoCount + '条数据没有查到，查询所用时间为：'+useTime+'</h3><br><a href = "http://10.236.91.57:3000'+filePath+'">点我下载</a><br/> <br/><a href = "http://10.236.91.57:3000">继续查询</a>');
+		request.post({
+		  headers: {'content-type' : 'text/plain;charset=UTF-8'},
+		  url:'https://wx.qq.com/cgi-bin/mmwebwx-bin/webwxsendmsg?lang=zh_CN&pass_ticket='+pass_ticket,
+		  json:json
+		}, function(error, response, body){
+			console.log('postMsg..........')
+		  console.log(body);
+		});
 	}
 });
 
