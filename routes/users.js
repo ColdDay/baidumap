c41865f009afac569e253f5fc2e225ac4020ff76
jinkai yang <jinkaiyang@miit-20160324wn.mioffice.cn>
2017-10-18 14:20:47 +0800

    设置后头信息

--- routes/users.js
+++ routes/users.js
@@ -21,6 +21,22 @@ var upload = multer({ storage: storage })
 
 /* GET home page. */
 router.get('/wxlogin', function(req, res, next) {
+
+	var headers = {
+		// 'Content-Type' : 'text/plain;charset=UTF-8',
+		'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36',
+		'Host':'wx.qq.com',
+		'Accept-Language':'zh-CN,en;q=0.8,zh;q=0.6,zh-TW;q=0.4,ja;q=0.2,id;q=0.2,ru;q=0.2',
+		'Connection':'keep-alive',
+		'Referer':'https://wx.qq.com/',
+		'DNT':'1'
+	}
+	var myUserName = '';
+	var pass_ticket = '';
+	var skey = '';
+	var sid = '';
+	var uin = '';
+	var deviceId = 'e'+Math.random().toFixed(15).substring(2,17)
 	getUUID();
 
 	function getUUID(){
@@ -65,31 +81,34 @@ router.get('/wxlogin', function(req, res, next) {
 	}
 
 	function getPassTicket(ticket,uuid){
-		
-		var hreq = https.get('https://wx.qq.com/cgi-bin/mmwebwx-bin/webwxnewloginpage?ticket='+ticket+'&uuid='+uuid+'&lang=zh_CN&scan=1508243070&fun=new&version=v2&lang=zh_CN',function(ress){  
-	    ress.setEncoding('utf-8');  
-	    var str = '';
-	    ress.on('end',function(){
-	    	console.log('请求PassTicket------->>>>>>>>');
-	    	var pass_ticket = str.split('<pass_ticket>')[1].split('</pass_ticket>')[0];
-	    	var skey = str.split('<skey>')[1].split('</skey>')[0];
-	    	var sid = str.split('<wxsid>')[1].split('</wxsid>')[0];
-	    	var uin = str.split('<wxuin>')[1].split('</wxuin>')[0];
-	    	console.log('PassTicket='+pass_ticket);
-	    	console.log('skey='+skey);
-	    	console.log('sid='+sid);
-	    	console.log('uin='+uin);
-	    	getWxData(pass_ticket,skey,sid,uin)
-		    //res.send(pass_ticket)
-	    });  
-	    ress.on('data',function(chunk){
-	    	str+=chunk;
-	    });  
-	 	});
+		request.get({
+		  url:'https://wx.qq.com/cgi-bin/mmwebwx-bin/webwxnewloginpage?ticket='+ticket+'&uuid='+uuid+'&lang=zh_CN&scan=1508243070&fun=new&version=v2&lang=zh_CN',
+		}, function(error, response, body){
+			console.log('请求PassTicket------->>>>>>>>');
+			var str = body.toString();
+    	pass_ticket = str.split('<pass_ticket>')[1].split('</pass_ticket>')[0];
+    	skey = str.split('<skey>')[1].split('</skey>')[0];
+    	sid = str.split('<wxsid>')[1].split('</wxsid>')[0];
+    	uin = str.split('<wxuin>')[1].split('</wxuin>')[0];
+    	setCookie(response.headers['set-cookie'])
+    	wxInit()
+		});
 	}
-	function getWxData(pass_ticket,skey,sid,uin){
+
+	function setCookie(cookies){
+		var wxuin = cookies[0].split(';')[0];
+		var wxsid = cookies[1].split(';')[0];
+		var wxloadtime = cookies[2].split(';')[0];
+		var webwx_data_ticket = cookies[4].split(';')[0];
+		var webwxuvid = cookies[5].split(';')[0];
+		var webwx_auth_ticket = cookies[6].split(';')[0];
+		var last_wxuin = cookies[0].split(';')[0];
+		var wxuin = cookies[0].split(';')[0];
+		headers['Cookie'] = 'MM_WX_NOTIFY_STATE=1; MM_WX_SOUND_STATE=1; wxuin='+wxuin+'; wxsid='+wxsid+'; wxloadtime='+wxloadtime+'; mm_lang=zh_CN; webwx_data_ticket='+webwx_data_ticket+'; webwxuvid='+webwxuvid+'; webwx_auth_ticket='+webwx_auth_ticket+'; login_frequency=1; last_wxuin='+wxuin;
+	}
+	function wxInit(){
 		var baseRequest = {
-			DeviceID:"e734118103793217",
+			DeviceID:deviceId,
 			Sid:sid,
 			Skey:skey,
 			Uin:uin
@@ -98,28 +117,49 @@ router.get('/wxlogin', function(req, res, next) {
 				"BaseRequest":baseRequest
 		}
 		request.post({
-		  headers: {'content-type' : 'text/plain;charset=UTF-8'},
+		  headers: headers,
 		  url:'https://wx.qq.com/cgi-bin/mmwebwx-bin/webwxinit?r=-713029383&pass_ticket='+pass_ticket,
 		  json:json
 		}, function(error, response, body){
-			console.log('getWxData..........')
-		  var myUserName = body.User.UserName;
-		  console.log(body.User);
-		  console.log(myUserName)
-		  getAllUsers(pass_ticket,skey);
-		  //postMsg(myUserName,'@b63f16520526d2fadcfaa4abf2d39a7a78458101f34eddefa69bb6aa712cc664','~',pass_ticket,baseRequest);
+			console.log('getWxData..........');
+		 	myUserName = body.User.UserName;
+		  statusNotify()
 		});
 	}
-	function getAllUsers(pass_ticket,skey){
-		console.log(pass_ticket,skey);
+	function statusNotify(){
+		console.log(headers);
+		var json = {
+			BaseRequest:{
+				DeviceID:deviceId,
+				Sid:sid,
+				Skey:skey,
+				Uin:uin
+			},
+			Code:3,
+			FromUserName:myUserName,
+			ToUserName:myUserName,
+			ClientMsgId:Date.now()
+		}
+		request.post({
+		  headers: headers,
+		  url:'https://wx.qq.com/cgi-bin/mmwebwx-bin/webwxstatusnotify?pass_ticket='+pass_ticket,
+		  json:json
+		}, function(error, response, body){
+			console.log('statusNotify..........');
+			console.log(body)
+		  getAllUsers();
+		});
+	}
+	function getAllUsers(){
 		request.get({
+			headers: headers,
 		  url:'https://wx.qq.com/cgi-bin/mmwebwx-bin/webwxgetcontact?lang=zh_CN&pass_ticket='+pass_ticket+'&r='+Date.now()+'&seq=0&skey='+skey
 		}, function(error, response, body){
 			console.log('getAllUsers..........')
 		  console.log(body);
 		});
 	}
-	function postMsg(myUserName,toUserName,msg,pass_ticket,baseRequest){
+	function postMsg(myUserName,toUserName,msg){
 		var json = {
 				BaseRequest:baseRequest,
 				Msg:{
